3a429e28aaaecf02e99ccef62fa16a66d2822310
Hansikha <bethisandhya.reddy@gmail.com>
2024-11-02 13:20:34 +0000

    initial commit

---
+++ Lesson 3/activity.py
@@ -0,0 +1,15 @@
+a = 9
+print (type (a))
+b = 1.55
+print (type(b))
+c = "print"
+print (type(c))
+d = True
+print (type(d))
+
+e = 5.55
+print (int (e))
+f = "9"
+print (float(f))
+g = 11
+print (float(g))
