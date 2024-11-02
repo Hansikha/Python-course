c8f0f264a061c4062e80e76448e65b94527d78dd
Hansikha <bethisandhya.reddy@gmail.com>
2024-11-02 12:43:06 +0000

    initial commit

---
+++ Lesson 2/Activity 2.py
@@ -0,0 +1,8 @@
+#Identifiers
+First_Name = "Hansikha "
+Last_Name = "Bethi "
+print ("My full name is " , First_Name, Last_Name)
+
+#Taking an input from the user
+Answer =input("What is your favourite hobby?")
+print("You entered : ", Answer)
---
+++ Lesson 2/Activity.py
@@ -0,0 +1,3 @@
+print ("Hello")
+print ("My age is : " , 11)
+print ("Welcome to codingal" , end= "!!!")
