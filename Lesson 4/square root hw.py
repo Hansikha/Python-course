52237b1055c71113e415fd9511d623f2c80046d2
Hansikha <bethisandhya.reddy@gmail.com>
2024-11-03 18:06:28 +0000

    initial commit

---
+++ Lesson 4/square root hw.py
@@ -0,0 +1,11 @@
+import  math
+print ( math.sqrt(1))
+print ( math.sqrt(4))
+print ( math.sqrt(9))
+print ( math.sqrt(16))
+print ( math.sqrt(25))
+print ( math.sqrt(36))
+print ( math.sqrt(49))
+print ( math.sqrt(64))
+print ( math.sqrt(81))
+print ( math.sqrt(100))
---
+++ PraticeFolder/PrintPractice.py
@@ -0,0 +1,7 @@
+print ("cat")
+a = "sadhya"
+print ("dog")
+a = "fish"
+b =  " are "  
+c = "cute"
+print (a + b + c)
