5a81798d81946d2934962b14131299957829dce4
Hansikha <bethisandhya.reddy@gmail.com>
2024-11-03 11:44:30 +0000

    initial commit

---
+++ Lesson 4/Activity.py
@@ -0,0 +1,19 @@
+tree1 = 25
+tree2 = 10 
+tree3 = 35
+tree4 = 15
+tree5 = 20
+
+sum = tree1 + tree2 + tree3 + tree4 + tree5 
+print (sum )
+average = sum / 5 
+print (average)
+
+
+#Activity 2
+
+English = int(input ("enter english marks "))
+Maths = int(input("enter math marks "))
+Total = English + Maths
+
+
