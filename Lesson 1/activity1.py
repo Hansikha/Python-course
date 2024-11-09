73469b7853d657933c37e360c3d12a285d68e389
Hansikha <bethisandhya.reddy@gmail.com>
2024-11-09 21:04:52 +0000

    initial commit

---
+++ Lesson 1/activity1.py
@@ -0,0 +1,14 @@
+a = 9
+print (type (a))
+b = "chick"
+print (type (b))
+c = 8.5
+print (type (c))
+d = True
+print (type (d))
+e = "fried chicken"
+
+#Counting the letters of a string
+print (len (e))
+print (len (b))
+
---
+++ PraticeFolder/cinammonMugCake.py
@@ -0,0 +1,25 @@
+"""x = 12.5
+x = int(x)
+print (x)
+
+y = 309
+y = float(y)
+print (y)
+
+h = "118"
+h = int(h)
+print (h)"""
+
+#moldy
+#print ("please enter your name")
+#print (input("what is your name : "))
+
+b = input("what is your hobbies: ")
+print (b)
+a = "crochet"
+if (a==b):
+    print("what is A ",a)
+    print("What is B: ",b)
+    print ("I like to crochet too")
+else :
+    print ("nice hobby")
