211013635778bc47cedec92406d7ca5bd6d4f55b
Hansikha <bethisandhya.reddy@gmail.com>
2024-11-09 11:56:20 +0000

    initial commit

---
+++ Lesson 5/summer times hw.py
@@ -0,0 +1,13 @@
+temp = 30
+if temp > 20 :
+    print ( " you can wear light and soft clothes " )
+
+else :
+    print ( " you need to wear a pullover and jacket " )
+
+temp = 15
+if temp < 20 :
+    print ( " you need to wear a pullover and jacket " )
+
+else :
+    print ( " you can wear light and soft clothes " )
