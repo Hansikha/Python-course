307e179355d409aebb076863d1e8dbe8cbd81956
Hansikha <bethisandhya.reddy@gmail.com>
2024-11-16 14:53:23 +0000

    initial commit

---
+++ Lesson 6/checking alphabets hw.py
@@ -0,0 +1,5 @@
+input ("enter a character" ) 
+if "character">="a" and "character"<="z" :
+    print ("This character is in the alphabet")
+else :
+    print ("This character is not in the alphabet")
