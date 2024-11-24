d49aae8c2d8149231e1633b5cbd91e66c94e1e6d
Hansikha <bethisandhya.reddy@gmail.com>
2024-11-24 11:43:16 +0000

    initial commit

--- Lesson 9/check age hw.py
+++ Lesson 9/check age hw.py
@@ -1,5 +1,5 @@
-user = a = 15
-if a > 10 and 20 > a :
+a = int (input("enter the age"))
+if a > 10 and a <= 20 :
     print (" the user's age is between 10 and 20")
 else:
     print (" the user's age is not between 10 and 20")
---
+++ MayoChicken.py
@@ -0,0 +1,7 @@
+Burger = input ("what is your name: ") 
+AgeOfChicken = int(input ("how old are u: "))
+print ( Burger, AgeOfChicken )
+if AgeOfChicken < 3 :
+    print (" You may eat the burger and not die")
+else:
+    print (" dont eat it or you may die")
