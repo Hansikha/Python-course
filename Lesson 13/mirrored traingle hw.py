3e0274b4814bf1c7eda460590f62afa8b228bff9
Hansikha <bethisandhya.reddy@gmail.com>
2024-12-14 19:42:00 +0000

    initial commit

---
+++ Lesson 13/mirrored traingle hw.py
@@ -0,0 +1,6 @@
+row = int (input("Please enter the amount of rows you want "))
+for i in range(row):
+     print (" "*i+"*"*(row - i))
+row2 = int (input("Please enter the amount of rows you want "))
+for i in range(row2):
+     print ("*"*(row2-i))
