3ff701fa5c7779b82717bf19caf196e3c946a75a
Hansikha <bethisandhya.reddy@gmail.com>
2024-12-15 11:41:46 +0000

    initial commit

---
+++ Lesson 15/functions.py
@@ -0,0 +1,17 @@
+def display ():
+    return 15 % 3
+print(display())
+#function with parametares
+def dis (name, age):
+    print ("the name is ", name)
+    print ("the age is ", age)
+dis("hansikha", 12)
+#function
+def multi (a, b):
+    mul = a*b
+    print (mul)
+
+c = 5
+d = 10
+multi (c, d)
+
