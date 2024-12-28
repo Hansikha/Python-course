c97c47e30c6edef371b5e622339fdce29784e6f2
Hansikha <bethisandhya.reddy@gmail.com>
2024-12-28 19:23:16 +0000

    initial commit

---
+++ Lesson 16/shutdown hw.py
@@ -0,0 +1,12 @@
+import os
+a = input("would you like to shut down your computer? yes/no: ") 
+if a == 'no':
+    exit()
+else: 
+    b = input("do you have any unsaved peices of work? yes/no: ")
+if b == 'yes':
+    exit()
+else:
+    os.system("shutdown /s /t 1") 
+
+    
---
+++ PraticeFolder/UberDriving.py
@@ -0,0 +1,14 @@
+#fruit activity
+a = "car"
+b = "bike"
+print("option a is",a)
+print("option b is",b)
+e = input("please pick what option you want to be your uber: ")
+if e is a:
+    print("what type of car do you want")
+c = input("please enter the type of car you want to drive: ")
+print("you will be driving in a",c)
+if e is b:
+    print("what type of bike do you want")
+d = input("please enter the type of bike you want to drive: ")
+print("you will be driving a",d)
