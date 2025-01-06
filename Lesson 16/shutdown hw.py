def756d84fb4f3f36716fd4d3c04ac43c0cfb81c
Hansikha <bethisandhya.reddy@gmail.com>
2025-01-06 18:23:10 +0000

    initial commit

--- Lesson 16/shutdown hw.py
+++ Lesson 16/shutdown hw.py
@@ -4,8 +4,8 @@ if a == 'no':
     exit()
 else: 
     b = input("do you have any unsaved peices of work? yes/no: ")
-if b == 'yes':
-    exit()
-else:
-    os.system("shutdown /s /t 1") 
+    if b == 'yes':
+        exit()
+    else:
+        os.system("shutdown /s /t 1") 
 
