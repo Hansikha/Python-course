3ecca0c64379c52c2b9daa29a62ef6c1bf4a1b94
Hansikha <bethisandhya.reddy@gmail.com>
2025-01-08 13:15:58 +0000

    initial commit

---
+++ Lesson 18/age counter hw.py
@@ -0,0 +1,13 @@
+a = int(input("Please enter your age: "))
+try:
+    if type(a) is not int:
+        raise ValueError
+    else:
+        if a % 2 == 0:
+            print("This number is an even number: ")
+        else:
+            print("This number is an odd number: ")
+finally:
+    print("The end")
+
+    
