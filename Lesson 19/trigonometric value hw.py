af2c0c9c9b997918599f438882008c0323d572aa
Hansikha <bethisandhya.reddy@gmail.com>
2025-01-12 11:33:51 +0000

    initial commit

---
+++ Lesson 19/trigonometric value hw.py
@@ -0,0 +1,5 @@
+import math
+a = int(input("Please enter the number you want to find the trigonometric value of: "))
+print("The sin of",a)
+math.sin(a)
+print(math.sin(a))
---
+++ Lesson 20/activity 1.py
@@ -0,0 +1,7 @@
+from datetime import date, time, datetime
+today=date.today()
+print(date.today())
+now=datetime.now()
+print(datetime.now())
+
+print("The date components are ",today.year, today.month, today.day)
