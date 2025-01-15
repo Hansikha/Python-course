497c58c5ee0078af8686c7a9226e3c2685db18a1
Hansikha <bethisandhya.reddy@gmail.com>
2025-01-15 17:50:00 +0000

    initial commit

---
+++ Lesson 21/activity 1.py
@@ -0,0 +1,13 @@
+#Making an empty list
+empty_list = []
+print(empty_list)
+#Making a list
+list = [1, 2, 3, 4, 5, 6]
+print(list)
+#Times operator 
+list2 = [1, 2, 3]*2
+print(list2)
+#Reversinng a list
+list3 = [100,200,300,400,500]
+list3 = list3[::-1]
+print(list3)
