427c3bf134c598fbf0975b55773c68e1d2159d3e
Hansikha <bethisandhya.reddy@gmail.com>
2025-01-19 21:19:12 +0000

    initial commit

---
+++ Lesson 21/calendar hw.py
@@ -0,0 +1,4 @@
+import calendar
+year = int(input("Please enter the year you want to find out the calendar of: "))
+month = int(input("Please enter the month you want to find out the calendar of: "))
+print(calendar.month(year,month))
