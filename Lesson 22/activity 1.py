2a18f141e2f9f73b42d795e324728dfc460f7e49
Hansikha <bethisandhya.reddy@gmail.com>
2025-01-19 11:42:26 +0000

    initial commit

---
+++ Lesson 22/activity 1.py
@@ -0,0 +1,16 @@
+#Tuples
+Tuples1 = ("1", 2, 3.5, "5","python")
+print (Tuples1)
+#Tuples with integers
+Tuples2 = (1, 2, 3, 4, 5, 6)
+print (Tuples2)
+#Concatenating tuples
+Tuples3 = Tuples2+(9,)
+print (Tuples3)
+#Using count method
+Tuples4 = (2, 4, 6, 8, 10, 10)
+print (Tuples4.count(10))
+#Performing slicing on a tuple
+print (Tuples4 [0:4])
+#Performing indexing on a tuple
+print (Tuples4 [1])
