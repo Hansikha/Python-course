d58cf8e0d42dfe40c1b8e67d5dd6e18bffadeaae
Hansikha <bethisandhya.reddy@gmail.com>
2025-01-26 21:30:48 +0000

    initial commit

--- Lesson 22/square it out hw.py
+++ Lesson 22/square it out hw.py
@@ -1,2 +1,18 @@
-list1 = {1,2,3,4,5,6,7,8,9,10}
-print (list1)
+def square_Value_and_filter(start, end):
+ odd_square =[]
+ even_square=[]
+#loop through the range
+ for num in range(start, end+1):
+    square = num**2
+
+    #call the square of the number
+    if square %2 ==0 :
+     even_square.append(square)
+ else:
+     odd_square.append(square)
+     print("odd square values: ", odd_square)
+     print("even square values", even_square)
+
+start_range = int(input("enter the begining number of the range: "))
+end_range = int(input("Please enter the ending number of the range: "))
+square_Value_and_filter(start_range, end_range)
--- Lesson 22/tuple product hw.py
+++ Lesson 22/tuple product hw.py
@@ -1,8 +1,6 @@
-Tup1 = (1, 2, 3, 4)
-
-
-Tup3 = Tup1 * Tup2
-print (Tup3)
-
+Tup1 =(2, 3, 4, 5, 6, 7)
+Mul = [(a * b) Tup1]
+print("The orignal list was: ",Tup1)
+print("The new list is ",Mul)
 
 
