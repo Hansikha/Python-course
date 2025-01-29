91ff86d69a2c64d69d5294736e63219dba0f09d8
Hansikha <bethisandhya.reddy@gmail.com>
2025-01-29 17:20:11 +0000

    initial commit

--- Lesson 22/tuple product hw.py
+++ Lesson 22/tuple product hw.py
@@ -2,10 +2,10 @@ def multiplying_tuple_number(numb_tuple):
     product = 1
     for numb in numb_tuple:
         product *= numb
-        return product
-    Tup1 = (1, 2, 3)
-    result = multiplying_tuple_number(Tup1)
-    print("The product of all numbers are",result)
+    return product
+Tup1 = (1, 2, 3)
+result = multiplying_tuple_number(Tup1)
+print("The product of all numbers are",result)
 
 
 
