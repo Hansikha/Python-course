78da869e88670f07aa38ff5de688695a7efb8891
Hansikha <bethisandhya.reddy@gmail.com>
2025-02-01 22:35:00 +0000

    initial commit

---
+++ Lesson 23/activity 1.py
@@ -0,0 +1,3 @@
+sd = {"ID1":{"Name":"Bob","Grade":"3","Subjects":["Maths","English"]}, 
+    "ID2":{"Name":"Sam","Grade":"5","Subjects":["Maths","English"]},
+    "ID3":{"Name":"Jake","Grade":"2","Subjects":["Maths","English"]}}
