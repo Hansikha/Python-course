6c9991b4c0a3a91e503ace43d36514cb7c75c5c9
Hansikha <bethisandhya.reddy@gmail.com>
2025-02-02 10:20:37 +0000

    initial commit

--- Lesson 22/square it out hw.py
+++ Lesson 22/square it out hw.py
@@ -7,11 +7,11 @@ def square_Value_and_filter(start, end):
 
     #call the square of the number
     if square %2 ==0 :
-     even_square.append(square)
- else:
-     odd_square.append(square)
-     print("odd square values: ", odd_square)
-     print("even square values", even_square)
+        even_square.append(square)
+    else:
+        odd_square.append(square)
+ print("odd square values: ", odd_square)
+ print("even square values", even_square)
 
 start_range = int(input("enter the begining number of the range: "))
 end_range = int(input("Please enter the ending number of the range: "))
