bf6c4883a32be791bf2f5d00d6c994185300c081
Hansikha <bethisandhya.reddy@gmail.com>
2025-02-02 10:55:23 +0000

    initial commit

--- Lesson 23/activity 1.py
+++ Lesson 23/activity 1.py
@@ -1,3 +1,12 @@
 sd = {"ID1":{"Name":"Bob","Grade":"3","Subjects":["Maths","English"]}, 
     "ID2":{"Name":"Sam","Grade":"5","Subjects":["Maths","English"]},
-    "ID3":{"Name":"Jake","Grade":"2","Subjects":["Maths","English"]}}
+    "ID3":{"Name":"Jake","Grade":"2","Subjects":["Maths","English"]},
+    "ID4":{"Name":"Jake","Grade":"2","Subjects":["Maths","English"]}}
+
+result = {}
+
+for key,value in sd.items():
+    if value not in result.values():
+        result[key] = value
+
+print(result)
---
+++ pop activity.py
@@ -0,0 +1,6 @@
+dictionary1 = {"Name":"Hansikha","Age":"12"}
+#dictionary1.pop("Name")
+#print(dictionary1)
+#print(dictionary1.get("Name"))
+#print(dictionary1.clear())
+print(dictionary1.copy())
