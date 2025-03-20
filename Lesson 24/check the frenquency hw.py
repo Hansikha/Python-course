baffaeb97e6d91bd0a6aed45eb0ce13fc3fb7200
Hansikha <bethisandhya.reddy@gmail.com>
2025-03-20 21:45:32 +0000

    initial commit

--- Lesson 24/check the frenquency hw.py
+++ Lesson 24/check the frenquency hw.py
@@ -1,12 +1,9 @@
 dict_1 = {"Codingal":3,"is":2,"best":2,"for":2,"Coding":1}
 print (dict_1)
-a = (input("Hi! Enter the value you want to check the frequency of: "))
-
+a = int(input("Hi! Enter the value you want to check the frequency of: "))
 b = 0
 for res in dict_1:
     print(res)
     if dict_1[res] == a:
         b = b + 1
         print("The frenquency of your choosen value is : " + str(b))
-    else:
-        
