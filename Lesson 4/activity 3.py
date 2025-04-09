20010cc12e18b5cec02bd6e571e063cdaed0d8f4
Hansikha <bethisandhya.reddy@gmail.com>
2025-04-09 14:05:37 +0100

    initial commit

---
+++ Lesson 4/activity 3.py
@@ -0,0 +1,14 @@
+print("Hello! Please enter achieved test scores")
+
+maths = int(input("maths :"))
+english = int(input("english :"))
+science = int(input("science :"))
+french = int(input("french :"))
+
+add = maths+english+science+french
+print("The average of all subjects is")
+
+percent = int(add/400)*100
+
+print("end=percentage mark = ")
+print(percent)
