6f54fbfbad30ecabe43bb5724f9daa57a9e54764
Hansikha <bethisandhya.reddy@gmail.com>
2025-05-05 12:10:57 +0100

    initial commit

--- Lesson 31/vechicle polymorphism hw.py
+++ Lesson 31/vechicle polymorphism hw.py
@@ -1,29 +1,25 @@
 #class BMW
 class BMW:
-    def fuel_type(self):
-       print("The fuel_type for BMW is diesel")
+   def fuel_type(self):
+      print("The fuel_type for BMW is diesel")
        
-    def max_speed(self):
-       print("The max_speed for BMW is 191mph")
+   def max_speed(self):
+      print("The max_speed for BMW is 191mph")
 
 #class ferrari
 class ferrari:
-    def fuel_type(self):
-       print("The fuel_type for ferrari is premium fuel")
+   def fuel_type(self):
+      print("The fuel_type for ferrari is premium fuel")
        
-    def max_speed(self):
-       print("The max_speed for ferrari is 211mph")
+   def max_speed(self):
+      print("The max_speed for ferrari is 211mph")
 #object creation
 obj_BMW = BMW()
 obj_ferrari = ferrari()
 
 #polymorphism
 for car in (obj_BMW, obj_ferrari):
-    car.fuel_type
-    car.max_speed
+    car.fuel_type()
+    car.max_speed()
         
-#printing
-print(obj_BMW.fuel_type)
-print(obj_ferrari.fuel_type)
-print(obj_BMW.fuel_type)
-print(obj_ferrari.max_speed)
+
