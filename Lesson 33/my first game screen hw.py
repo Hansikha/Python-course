a1f788820eb7a139c3ae1d4eadb0f4fd1a6231a1
Hansikha <bethisandhya.reddy@gmail.com>
2025-05-06 21:55:58 +0100

    initial commit

--- Lesson 33/my first game screen hw.py
+++ Lesson 33/my first game screen hw.py
@@ -1,9 +1,28 @@
 import pygame
+
 pygame.init()
+screen = pygame.display.set_mode((500,500))
+caption = pygame.display.set_caption("MY FRIST GAME SCREEN")
+done = False
 
-pygame.display.set_mode((500,500))
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+    pygame.draw.rect(screen,(200,155,207),pygame.Rect(200, 250, 100, 50),0)
+    pygame.display.flip()
+    pygame.draw.rect(screen,(200,155,207),pygame.Rect(200, 250, 100, 50),0)
+    color = (58, 58, 58)
+ 
+    screen.fill(color)
+    pygame.display.flip()
+    
 
-caption = pygame.font.Font(None,30).render("MY FIRST GAME SCREEN", True,pygame.Color("white"))
+    
+    
+
+
+
 
 
 
